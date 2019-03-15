415b04550681eba1509628a1fcb3bf0091e4b62b
jdgalviss <ANpame233903>
2019-03-14 21:07:49 -0500

    first commit

---
+++ 11ConvNets/maxPooling.py
@@ -0,0 +1,10 @@
+...
+conv_layer = tf.nn.conv2d(input, weight, strides=[1, 2, 2, 1], padding='SAME')
+conv_layer = tf.nn.bias_add(conv_layer, bias)
+conv_layer = tf.nn.relu(conv_layer)
+# Apply Max Pooling
+conv_layer = tf.nn.max_pool(
+    conv_layer,
+    ksize=[1, 2, 2, 1],
+    strides=[1, 2, 2, 1],
+    padding='SAME')
